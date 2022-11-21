37d7833e84fb32080e03c0ed326de56b1535d5e8
ix-kuboki <88076325+ix-kuboki@users.noreply.github.com>
2022-11-21 11:09:45 +0900

    (Vue)ルーティングなどの下準備

--- client/src/router.js
+++ client/src/router.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/MyHome.vue";
-import Users from "./views/MyUsers.vue";
 import Test from "./views/MyTest.vue";
+import TaskIndex from "@/pages/task/TaskIndex.vue";
+import TaskCreate from "@/pages/task/TaskCreate.vue";
+import UserIndex from "@/pages/user/UserIndex.vue";
 
 
 Vue.use(Router);
@@ -10,9 +11,11 @@ Vue.use(Router);
 export default new Router({
     mode: "history",
     routes: [
-        {path: '/', component: Home},
-        {path: '/users', component: Users},
-        {path: '/mytest', component: Test},
+        {path: '/', component: TaskIndex},
+        {path: '/task/create', component: TaskCreate},
 
+        {path: '/user/index', component: UserIndex},
+        {path: '/mytest', component: Test},
+        {path: '*', redirect: '/'},
     ]
 });
