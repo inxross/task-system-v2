6cda0a3fa65bd844eadf446bd1bfd8dddcaa95ba
ix-kuboki <88076325+ix-kuboki@users.noreply.github.com>
2022-12-15 16:41:57 +0900

    （検索機能試行中）エラー 「Error in render: "TypeError: tentativeData.filter is not a function"」

--- client/src/pages/task/TaskIndex.vue
+++ client/src/pages/task/TaskIndex.vue
@@ -43,17 +43,30 @@ export default {
 
             //console.log(this.$route.query.category);
 
-            if ((this.$route.query.category == 0 && this.$route.query.status == 0 && this.$route.query.user == 0) || (!this.$route.query.category && !this.$route.query.status && !this.$route.query.user)) {
+            if ((this.$route.query.category == 0 && this.$route.query.status == 0 && this.$route.query.user == 0 && this.$route.query.searchword == '') || (!this.$route.query.category && !this.$route.query.status && !this.$route.query.user && !this.$route.query.searchword)) {
                 console.log(getters);
                 return getters;
             } else {
                 const categoryDataId = parseInt(this.$route.query.category, 10);
                 const statusDataId = parseInt(this.$route.query.status, 10);
                 const userDataId = parseInt(this.$route.query.user, 10);
+                const searchword = this.$route.query.searchword;
+                console.log(searchword);
 
                 const data = getters.filter( function(a) {
 
-                    return (categoryDataId==0 ? a.category_id !== categoryDataId : a.category_id == categoryDataId) && (statusDataId==0 ? a.status_id !== statusDataId : a.status_id == statusDataId) && (userDataId==0 ? a.work_user.id !== userDataId : a.work_user.id == userDataId)
+                    const tentativeData = (categoryDataId==0 ? a.category_id !== categoryDataId : a.category_id == categoryDataId) && (statusDataId==0 ? a.status_id !== statusDataId : a.status_id == statusDataId) && (userDataId==0 ? a.work_user.id !== userDataId : a.work_user.id == userDataId);
+
+                    if( searchword=='' || !searchword ) {
+                        const addSearchWordData = tentativeData.filter( function(b) {
+                            console.log('addSearchWordDataの処理を通っている');
+                            return b.name.match(searchword);
+                        })
+                        return addSearchWordData;
+                    } else {
+                        console.log('tentativeDataの処理を通っている');
+                        return tentativeData;
+                    }
 
                 })
 
