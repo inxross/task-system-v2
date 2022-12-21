5e667ab6da96e4166d6b0d4be4769b8369d60750
ix-kuboki <88076325+ix-kuboki@users.noreply.github.com>
2022-12-21 15:42:04 +0900

    ページネーション設定

--- client/src/main.js
+++ client/src/main.js
@@ -9,10 +9,14 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import VuePaginate from 'vuejs-paginate';
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
+
+Vue.component('VuePaginate', VuePaginate);
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.baseURL = "http://vue-laravel-separately-tasksystem.localdomain";
